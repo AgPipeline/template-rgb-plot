15e9245c8b6555d6e7640f71bcbe2bc786b58092
Chris Schnaufer <schnaufer@email.arizona.edu>
2019-11-22 14:14:17 -0700

    Added variable units and labels

--- algorithm_rgb.py
+++ algorithm_rgb.py
@@ -11,6 +11,7 @@ VERSION = '1.0'
 # Information on the creator of this algorithm
 ALGORITHM_AUTHOR = 'Unknown'
 ALGORITHM_AUTHOR_EMAIL = ''
+ALGORITHM_CONTRIBUTORS = ["Zongyang Li"]
 
 ALGORITHM_NAME = 'my nifty one'
 ALGORITHM_DESCRIPTION = 'This algorithm calculates the niftyness of RGB plot-level images'
@@ -25,7 +26,17 @@ CITATION_YEAR = ''
 # Note that variable names cannot have comma's in them: use a different separator instead. Also,
 # all white space is kept intact; don't add any extra whitespace since it may cause name comparisons
 # to fail.
+# !! Replace the content of this string with your variable names
 VARIABLE_NAMES = 'size of image channels'
+
+# Variable units matching the order of VARIABLE_NAMES, also comma-separated.
+# For each variable name in VARIABLE_NAMES add the unit of measurement the value represents.
+# !! Replace the content of this string with your variables' unit
+VARIABLE_UNITS = 'pixels'
+
+# Variable labels matching the order of VARIABLE_NAMES, also comma-separated.
+# This is an optional definition and can be left empty.
+VARIABLE_LABELS = ''
 
 # Optional override for the generation of a BETYdb compatible csv file
 # Set to False to suppress the creation of a compatible file
